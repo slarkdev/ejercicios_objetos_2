74a2a82069d317fcaa6e59c7d8c1e5da75cfc0ac
Lesly <72.31.lesly.she@gmail.com>
2017-06-26 15:28:46 -0500

    ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+function SuperCoder(nombre, edad, ocupacion, genero) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.ocupacion = ocupacion;
+    this.genero = genero;
+    this.mensaje = function() {
+        return (this.edad > 17 &&
+            (this.ocupacion.toLowerCase() == "web developer" ||
+                this.ocupacion.toLowerCase() == "estudiante laboratoria")) ? "You're awesome" : "Upsiii";
+    }
+}
+
+describe("Ejercicio 2", function() {
+    it("para Tamara deberia ser You're awesome", function() {
+        var coder = new SuperCoder("Tamara", 22, "Estudiante Laboratoria", "Femenino");
+        assert.equal("You're awesome", coder.mensaje());
+    })
+    it("para Ester deberia ser Upsiii", function() {
+        var coder = new SuperCoder("Ester", 22, "Enfermera", "Femenino");
+        assert.equal("Upsiii", coder.mensaje());
+    })
+})
