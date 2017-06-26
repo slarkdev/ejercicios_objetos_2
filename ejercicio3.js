18f760f129d4c73b7e604939d3beb49480e95f37
Lesly <72.31.lesly.she@gmail.com>
2017-06-26 16:16:18 -0500

    actualizacion ejercicio 3

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,6 +1,13 @@
+// Escriba una funcion que reciba como parametro un arreglo y que sus elementos 
+// sean agregados a un objeto llamado texto retornar las propiedad encadenadas en un string
+
 function aObjeto(array) {
-    return array.reduce(function(total, actual, indice) {
+    var res = '';
+    var texto = array.reduce(function(total, actual, indice) {
         total["propiedad" + (indice + 1)] = actual;
+        res += "propiedad" + (indice + 1) + "-->" + actual + ";";
         return total;
     }, {});
+    return res;
 }
+console.log(aObjeto([6, 5, 4, 3, 2, 1]));
